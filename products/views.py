3411beb3ca620a9f2ed012d30a427ee725151584
Rober <boblawyer1@gmail.com>
2023-05-25 13:49:43 -0300

    vista detalle producto

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.shortcuts import render
 from .models import Product
 
@@ -12,3 +12,13 @@ class ProductListView(ListView):
         context = super().get_context_data(**kwargs)
         context['message']= 'Listado de Productos' 
         return context
+    
+class ProductDetailView(DetailView):
+    model = Product
+    template_name  = 'products/product.html'
+    
+    def get_context_data(self, **kwargs):
+
+        context = super().get_context_data(**kwargs)
+        return context
+
--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 from products.views import ProductListView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='index'),
     path('admin/', admin.site.urls),
+    path('productos/', include('products.urls')),
     path('usuarios/login/', views.login_view, name='login'),
     path('usuarios/logout/', views.logout_view, name='logout'),
     path('usuarios/register/', views.register_view, name='register'),
