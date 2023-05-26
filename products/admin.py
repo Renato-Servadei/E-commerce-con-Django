6e0d2adbcd5d350f4f1dca5f38d6e0a10fc9137d
Rober <boblawyer1@gmail.com>
2023-05-26 16:00:26 -0300

    agregado el campo imagen al modelo product y modif. para ver imgs

--- products/admin.py
+++ products/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Product
 
 class ProductAdmin(admin.ModelAdmin):
-    fields = ('title', 'description', 'price')
+    fields = ('title', 'description', 'image', 'price')
     list_display = ('__str__', 'slug', 'created')
 
 
--- store/urls.py
+++ store/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from . import views
@@ -11,3 +13,6 @@ urlpatterns = [
     path('usuarios/logout/', views.logout_view, name='logout'),
     path('usuarios/register/', views.register_view, name='register'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
