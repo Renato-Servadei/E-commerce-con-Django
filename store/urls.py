9636376c8393b58fac7346af29a5210aae087967
Rober <boblawyer1@gmail.com>
2023-05-27 14:59:11 -0300

    vista cart y template

--- store/urls.py
+++ store/urls.py
@@ -8,6 +8,7 @@ from products.views import ProductListView
 urlpatterns = [
     path('', ProductListView.as_view(), name='index'),
     path('admin/', admin.site.urls),
+    path('carrito/', include('carts.urls')),
     path('productos/', include('products.urls')),
     path('usuarios/login/', views.login_view, name='login'),
     path('usuarios/logout/', views.logout_view, name='logout'),
