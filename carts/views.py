378699b4877cc49d94e85f6f7ae290da000caf54
Rober <boblawyer1@gmail.com>
2023-05-28 13:32:09 -0300

    vista, template y logica agregar productos

--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Cart
+from products.models import Product
 from .utils import get_or_create_cart
 
 
@@ -7,4 +7,13 @@ def cart(request):
     
     cart = get_or_create_cart(request)
 
-    return render(request, 'carts/cart.html', {'cart': cart})
+    return render(request, 'carts/cart.html', {})
+
+def add(request):
+
+    cart = get_or_create_cart(request)
+    product = Product.objects.get(pk=request.POST.get('product_id'))
+
+    cart.products.add(product)
+
+    return render(request, 'carts/add.html', {'product': product})
