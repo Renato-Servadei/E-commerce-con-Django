9bb8099074803554f052887c7c5bf53bbbe488ab
Rober <boblawyer1@gmail.com>
2023-06-01 12:44:56 -0300

    actualizar cantidad de productos agregados, calcular subtotal

--- carts/models.py
+++ carts/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.signals import pre_save, m2m_changed
+from django.db.models.signals import pre_save, m2m_changed, post_save
 from products.models import Product
 from users.models import User
 import uuid
@@ -23,7 +23,13 @@ class Cart(models.Model):
         self.update_total()
 
     def update_subtotal(self):
-        self.subtotal = sum([products.price for products in self.products.all()])
+        # self.subtotal = sum([products.price for products in self.products.all()])
+        # self.save()
+
+        # actualizamos el calculo del subtotal multiplicando la cantidad de cada producto por el precio
+        self.subtotal = sum([
+            cp.quantity * cp.product.price for cp in self.products_related()
+        ]) 
         self.save()
 
     def update_total(self):
@@ -33,11 +39,27 @@ class Cart(models.Model):
     def products_related(self):
         return self.cartproducts_set.select_related('product')
 
+class CartProductsManager(models.Manager):
+    #con esta clase obtnemos o creamos un objeto cart. si ya esta creado se actualiza la cantida del product
+    def create_or_update_quantity(self, cart, product, quantity=1):
+        object, created = self.get_or_create(cart=cart, product=product)
+
+        if not created:
+            quantity = object.quantity + quantity
+        object.update_quantity(quantity)    
+        return object
+
 class CartProducts(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
     created_at = models.DateTimeField(auto_now_add=True)
+    #lo traemos de cartProductsManager para actualizar la cantidad de producto
+    objects = CartProductsManager()
+
+    def update_quantity(self, quantity = 1):
+        self.quantity = quantity
+        self.save()
 
 def set_cart_id(sender, instance, *args, **kwargs):
     if not instance.cart_id:
@@ -47,6 +69,10 @@ def update_totals(instance, sender, action, *args, **kwargs):
     if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
         instance.update_totals()
 
+def post_save_update_totals(instance, sender, *args, **kwargs):
+        instance.cart.update_totals()
+
 pre_save.connect(set_cart_id, sender=Cart)
+post_save.connect(post_save_update_totals, sender=CartProducts)
 m2m_changed.connect(update_totals, sender=Cart.products.through)
 
--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from products.models import Product
+from .models import CartProducts
 from .utils import get_or_create_cart
 
 
@@ -13,13 +14,21 @@ def add(request):
 
     cart = get_or_create_cart(request)
     product = get_object_or_404(Product, pk=request.POST.get('product_id'))
-    quantity = request.POST.get('quantity', 1)
+    quantity = int(request.POST.get('quantity', 1))
 
-    cart.products.add(product, through_defaults={
-        'quantity': quantity
-    })
-
-    return render(request, 'carts/add.html', {'product': product})
+    # cart.products.add(product, through_defaults={
+    #     'quantity': quantity
+    # })
+    #implementamos otra forma de crear la relacion entre carrito y productos
+    cart_product = CartProducts.objects.create_or_update_quantity(cart=cart, product=product, quantity=quantity)
+    #al hacerlo así se crea un nuevo item en el carrito (o sea que si agregamos más cantidad de un producto que
+    # ya estaba en el carrito lo vamos a ver dos veces, por eso seguimos modificando el archivo .models creando
+    # la clase CartProductManager)
+    return render(request, 'carts/add.html', {
+        'product': product,
+        'cart_product' : cart_product,
+        'quantity': quantity,
+        })
 
 def remove(request):
     cart = get_or_create_cart(request)
