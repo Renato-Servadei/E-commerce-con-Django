12ded1085d4c87049d293dd664380a1d9f786e40
Rober <boblawyer1@gmail.com>
2023-06-07 20:27:34 -0300

    loqin_required para la vista order

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,10 @@
 from carts.utils import get_or_create_cart
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import Order
 from .utils import get_or_create_order
 
+@login_required(login_url='login')
 def order(request):
 
     cart = get_or_create_cart(request)
