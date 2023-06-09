f91a56a63f38ec57cb76e60f0dce971897dadf46
Rober <boblawyer1@gmail.com>
2023-06-09 18:22:04 -0300

    mostrar lista de direcciones. proteccion de rutas

--- shipping_addresses/views.py
+++ shipping_addresses/views.py
@@ -1,10 +1,14 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
 from django.views.generic import ListView
 from .forms import ShippingAddressForm
 from .models import ShippingAddress
 
 
-class ShippingAddressListView(ListView):
+class ShippingAddressListView(LoginRequiredMixin, ListView):
+    login_url = 'login'
     model = ShippingAddress
     template_name = 'shipping_addresses/shipping_addresses.html'
 
@@ -12,6 +16,16 @@ class ShippingAddressListView(ListView):
         return ShippingAddress.objects.filter(user=self.request.user).order_by('-default')
 # Create your views here.
 
+@login_required(login_url='login')
 def create(request):
-    form = ShippingAddressForm()
+    form = ShippingAddressForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        shipping_address = form.save(commit=False)
+
+        shipping_address.user = request.user
+        shipping_address.default = not ShippingAddress.objects.filter(user=request.user).exists()
+        shipping_address.save()
+        messages.success(request, 'Dirección de envío agregada exitosamente')
+        return redirect('shipping_addresses:shipping_addresses')
     return render(request, 'shipping_addresses/create.html', {'form': form})
