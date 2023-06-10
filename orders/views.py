f38384f1617096910d62fe684844bf9bb9dd1337
Rober <boblawyer1@gmail.com>
2023-06-10 12:21:12 -0300

    seleccionar direccion de entrega

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,8 @@
 from carts.utils import get_or_create_cart
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+
+from shipping_addresses.models import ShippingAddress
 from .models import Order
 from .utils import get_or_create_order, breadcrumb
 
@@ -27,3 +29,25 @@ def address(request):
         'breadcrumb': breadcrumb(address=True),
         'shipping_address': shipping_address
     })
+
+@login_required(login_url='login')
+def select_address(request):
+    shipping_addresses = request.user.shippingaddress_set.all()
+
+    return render(request, 'orders/select_address.html', {
+        'breadcrumb': breadcrumb(address=True),
+        'shipping_addresses': shipping_addresses
+    })
+
+@login_required(login_url='login')
+def check_address(request, pk):
+    cart = get_or_create_cart(request)
+    order = get_or_create_order(cart, request)
+
+    shipping_address = get_object_or_404(ShippingAddress, pk=pk)
+
+    if request.user.id != shipping_address.user.id:
+        return redirect('index')
+    
+    order.update_shipping_address(shipping_address)
+    return redirect('orders:address')
